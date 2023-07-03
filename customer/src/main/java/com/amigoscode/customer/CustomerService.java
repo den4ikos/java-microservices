03e205ba49b80eec0422013716a92d42efe4c9be
den4ikos <denasius89@yandex.ru>
2023-07-03 15:46:02 +0300

    added open fiegn service

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -1,5 +1,7 @@
 package com.amigoscode.customer;
 
+import com.amigoscode.clients.fraud.FraudCheckResponse;
+import com.amigoscode.clients.fraud.FraudClient;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 public class CustomerService {
     private final CustomerRepository customerRepository;
     private final RestTemplate restTemplate;
+    private final FraudClient fraudClient;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -19,11 +22,9 @@ public class CustomerService {
 
         customerRepository.saveAndFlush(customer);
 
-        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://FRAUD/api/v1/fraud-check/{customerId}",
-                FraudCheckResponse.class,
-                customer.getId()
-        );
+        FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
+
+
 
         if (fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("FRAUDSTER");
--- fraud/src/main/java/com/amigoscode/fraud/FraudController.java
+++ fraud/src/main/java/com/amigoscode/fraud/FraudController.java
@@ -1,8 +1,11 @@
 package com.amigoscode.fraud;
 
+import com.amigoscode.clients.fraud.FraudCheckResponse;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/api/v1/fraud-check")
 @AllArgsConstructor
@@ -12,6 +15,7 @@ public class FraudController {
     @GetMapping(path = "/{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
         Boolean isFraudCheckService = fraudCheckService.isFraudulentCustomer(customerId);
+        log.info("fraud check response for customer {}", customerId);
         return new FraudCheckResponse(isFraudCheckService);
     }
 }
